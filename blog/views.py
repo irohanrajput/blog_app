fb4695041d38a632e2397e05412b940244975b6f
irohanrajput <itusharsingh27@gmail.com>
2023-12-01 18:36:04 +0530

    configuring pycharm

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'blog/home.html'
+    context_object_name = 'all_posts_list'
+    # if in case the context object name is not defined, the default name is object_list
 
 class BlogDetailView(DetailView):
     model = Post
